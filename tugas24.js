79c50876fa3c4047b56a09a3f3a8b1d96e8fc874
Hanif-Niomic <m.alihanif249@gmail.com>
2022-04-04 21:44:29 +0700

    tugas java
    tugas java

---
+++ tugas24.js
@@ -0,0 +1,10 @@
+function tugas24(){
+  var deret = [32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66];
+  console.log("Sebelumnya : ",deret.toString());
+  urut = deret.sort();
+  console.log("Ascending : ",urut.toString());
+  reverse = urut.reverse();
+  console.log("Descending : ",reverse.toString());
+}
+
+tugas24()
